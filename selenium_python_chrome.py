52da453f0ee22d4b2a7abedb02ca1454022b598d
red5191 <the_red51@outlook.com>
2025-01-18 01:42:00 +0300

    Initial commit

---
+++ selenium_python_chrome.py
@@ -0,0 +1,28 @@
+# Прописываем в терминале:
+# python -m pip install --upgrade pip (Обновление менеджера пакетов pip)
+# pip install selenium (Устанавливаем библиотеку selenium)
+# pip install webdriver-manager (Устанавливаем webdriver-manager)
+import time
+
+# импортируем необходимые библиотеки и элементы
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+# создаем и настраиваем экземпляр driver класса webdriver
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+# options.add_argument('--headless')
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
+# создаем переменную содержащую базовую ссылку и открываем её с помощью созданного ранее driver
+base_url = 'http://demoqa.com/radio-button'
+driver.get(base_url)
+driver.maximize_window()
+
+radio_button = driver.find_element(By.XPATH, "(//label[@class='custom-control-label'])[2]")
+radio_button.click()
+not radio_button.is_selected()
+print('Radio Button Active')
